ddb4347e307c63f0c7c9f895a691588614a9d59c
Bayu Novaldi <36811359+idlavoNuyaB@users.noreply.github.com>
2021-12-11 06:49:17 +0700

    add font, & reset activity. change layout main activity

---
+++ app/src/main/java/com/example/alodokter_rakamin_android_kelompok1/view/booking/BookingFragment.kt
@@ -0,0 +1,32 @@
+package com.example.alodokter_rakamin_android_kelompok1.view.booking
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.alodokter_rakamin_android_kelompok1.R
+
+class BookingFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = BookingFragment()
+    }
+
+    private lateinit var viewModel: BookingViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.booking_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this).get(BookingViewModel::class.java)
+
+    }
+
+}
---
+++ app/src/main/java/com/example/alodokter_rakamin_android_kelompok1/view/booking/BookingViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.alodokter_rakamin_android_kelompok1.view.booking
+
+import androidx.lifecycle.ViewModel
+
+class BookingViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
