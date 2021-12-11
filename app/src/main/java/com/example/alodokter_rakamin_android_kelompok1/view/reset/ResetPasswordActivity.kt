c928d6c293e25b5819b6e0f56b560b31abd05ee9
Bayu Novaldi <36811359+idlavoNuyaB@users.noreply.github.com>
2021-12-11 09:44:21 +0700

    add font, & reset activity. change layout main activity

---
+++ app/src/main/java/com/example/alodokter_rakamin_android_kelompok1/view/reset/ResetPasswordActivity.kt
@@ -0,0 +1,84 @@
+package com.example.alodokter_rakamin_android_kelompok1.view.reset
+
+import android.content.res.ColorStateList
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageButton
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.textfield.TextInputEditText
+import androidx.core.content.ContextCompat
+import androidx.core.widget.doAfterTextChanged
+import androidx.lifecycle.ViewModelProvider
+import com.example.alodokter_rakamin_android_kelompok1.R
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputLayout
+
+class ResetPasswordActivity : AppCompatActivity() {
+
+    private lateinit var textInput: TextInputEditText
+    private lateinit var btnSubmit: MaterialButton
+    private lateinit var textLayout: TextInputLayout
+    private lateinit var toolbar : Toolbar
+    private lateinit var back : ImageButton
+    private lateinit var viewModel: ResetPasswordViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_reset_password)
+//        window.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS)
+        toolbar = findViewById(R.id.toolbar)
+        back = findViewById(R.id.back)
+        btnSubmit = findViewById(R.id.btn_submit)
+        textInput = findViewById(R.id.text_input)
+        textLayout = findViewById(R.id.text_input_field)
+        viewModel = ViewModelProvider(this)[ResetPasswordViewModel::class.java]
+        setToolbar()
+        textInput.doAfterTextChanged {
+            viewModel.afterChangePassword(it,resources.getString(R.string.not_email_address))
+        }
+        viewModel.isButtonEnabled.observe(this) {
+            btnSubmit.isEnabled = it
+        }
+        viewModel.isHelperEnabled.observe(this){
+            textLayout.isHelperTextEnabled = it
+        }
+        viewModel.isErrorEnabled.observe(this){
+            textLayout.isErrorEnabled = it
+        }
+        viewModel.textError.observe(this){
+            textLayout.error = it
+        }
+        viewModel.countdownTimer.observe(this){
+            if(it > 0){
+                val time = it / 1000
+                textLayout.helperText = resources.getString(R.string.resend_email_timer,time)
+            } else {
+                textLayout.helperText = null
+            }
+        }
+        viewModel.isResend.observe(this){
+            if(it){
+                val color = ContextCompat.getColor(applicationContext, R.color.black)
+                val colorStateList = ColorStateList.valueOf(color)
+                textLayout.setHelperTextColor(colorStateList)
+            }
+        }
+        btnSubmit.setOnClickListener {
+            viewModel.countDown().start()
+            viewModel.sendEmail()
+        }
+        val btnSignUp = findViewById<MaterialButton>(R.id.btn_sign_up)
+        btnSignUp.setOnClickListener {
+//            val intent = Intent(applicationContext,::class.java)
+//            startActivity(intent)
+        }
+    }
+
+    private fun setToolbar(){
+        supportActionBar?.hide()
+        back.setOnClickListener {
+            viewModel.countDown().cancel()
+            finish()
+        }
+    }
+}
---
+++ app/src/main/java/com/example/alodokter_rakamin_android_kelompok1/view/reset/ResetPasswordViewModel.kt
@@ -0,0 +1,72 @@
+package com.example.alodokter_rakamin_android_kelompok1.view.reset
+
+import android.os.CountDownTimer
+import android.text.Editable
+import android.util.Patterns
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+class ResetPasswordViewModel : ViewModel() {
+
+    private val _isButtonEnabled : MutableLiveData<Boolean> = MutableLiveData(false)
+    val isButtonEnabled : LiveData<Boolean> = _isButtonEnabled
+
+    private val _isHelperEnabled: MutableLiveData<Boolean> = MutableLiveData(false)
+    val isHelperEnabled : LiveData<Boolean> = _isHelperEnabled
+
+    private val _isErrorEnabled: MutableLiveData<Boolean> = MutableLiveData(false)
+    val isErrorEnabled : LiveData<Boolean> = _isErrorEnabled
+
+    private val _countdownTimer: MutableLiveData<Int> = MutableLiveData(0)
+    val countdownTimer : LiveData<Int> = _countdownTimer
+
+    private val _textError: MutableLiveData<String?> = MutableLiveData()
+    val textError: LiveData<String?> = _textError
+
+    private val _isResend: MutableLiveData<Boolean> = MutableLiveData(false)
+    val isResend: LiveData<Boolean> = _isResend
+
+    private var email: String = ""
+
+    fun sendEmail(){
+        viewModelScope.launch(Dispatchers.IO) {
+            //auth
+        }
+    }
+
+    fun afterChangePassword(it: Editable?,errorText: String){
+        if(Patterns.EMAIL_ADDRESS.matcher(it.toString()).matches()){
+            _isButtonEnabled.value = true
+            _textError.value = null
+            _isErrorEnabled.value = false
+            email = it.toString().trim()
+        } else {
+            _isButtonEnabled.value = false
+            _textError.value = errorText
+            _isErrorEnabled.value = true
+            email = ""
+        }
+    }
+
+    fun countDown() : CountDownTimer {
+        val countDownTimer = object : CountDownTimer(30000,1000){
+            override fun onTick(millisUntilFinished: Long) {
+                _countdownTimer.value = millisUntilFinished.toInt()
+                _isHelperEnabled.value = true
+                _isButtonEnabled.value = false
+                _isResend.value = true
+            }
+
+            override fun onFinish() {
+                _isHelperEnabled.value = false
+                _isButtonEnabled.value = true
+                _isResend.value = false
+            }
+        }
+        return countDownTimer
+    }
+}
