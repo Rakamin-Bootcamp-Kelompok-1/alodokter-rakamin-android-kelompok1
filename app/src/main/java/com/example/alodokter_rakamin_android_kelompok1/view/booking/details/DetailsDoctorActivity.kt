10cfc5495552c79e5f74e6627736d0f494e05ab0
Faykar <faikarsummon11@gmail.com>
2021-12-12 15:30:53 +0700

    layout : details_doctor, confirm_booking, success_booking, item_specialist & item_doctor

---
+++ app/src/main/java/com/example/alodokter_rakamin_android_kelompok1/view/booking/details/DetailsDoctorActivity.kt
@@ -0,0 +1,14 @@
+package com.example.alodokter_rakamin_android_kelompok1.view.booking.details
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.alodokter_rakamin_android_kelompok1.R
+
+class DetailsDoctorActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_details_doctor)
+
+
+    }
+}
